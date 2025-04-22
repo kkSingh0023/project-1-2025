da30e29469ecec8ac89244a82ece1caa967351b4
kartikey <kartikeysingh892@gmail.com>
2025-04-22 23:19:44 +0530

    Upgrading for liteLLM

--- index.py
+++ index.py
@@ -1,28 +1,20 @@
-import openai
+
+from litellm import completion
 import os
 from dotenv import load_dotenv
+# Load environment variables from the .env file
 load_dotenv()
-# Set your API key
-client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def ask_question():
-    question = input("Ask a question: ")
+## set ENV variables
+os.environ["OPENAI_API_KEY"] =os.getenv("OPENAI_API_KEY")
+os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
 
-    try:
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": question}
-            ],
-            temperature=0.7,
-            max_tokens=300
-        )
+prompt=input("Enter anything for ur assistant")
+messages = [{"role": "system", "content": "You are a helpful assistant."},{ "content": prompt,"role": "user"}]
 
-        print("\nAssistant:", response.choices[0].message.content)
+# openai call
+# response = completion(model="openai/gpt-4o", messages=messages)
 
-    except Exception as e:
-        print("Error:", e)
-
-if __name__ == "__main__":
-    ask_question()
+# gemini call
+response = completion(model="gemini/gemini-2.0-flash", messages=messages)
+print(response.choices[0].message.content)
